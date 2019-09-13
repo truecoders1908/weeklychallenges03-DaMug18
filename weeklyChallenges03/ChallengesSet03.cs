ec7211a691382750794880262095cd2339d796b5
Ryan Smith <Rwsmith8692@gmail.com>
2019-09-13 16:35:45 -0500

    Weekly Challenges #3

--- weeklyChallenges03/ChallengesSet03.cs
+++ weeklyChallenges03/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 
 
@@ -10,37 +11,58 @@ namespace WeeklyChallenges03
     {
             public bool ArrayContainsAFalse(bool[] vals)
             {
-                throw new NotImplementedException();
-            }
+                foreach (bool x in vals)
+                {
+                if (x == false)
+                    return true;
 
+                }
+            return false;
+            }
+            
             public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
             {
-                throw new NotImplementedException();
+            if (numbers == null)
+                return false;
+            int x = numbers.Sum();
+            if (x % 2 == 0)
+                return false;
+            else
+                return true;
             }
 
-            public bool PasswordContainsUpperLowerAndNumber(string password)
+        public bool PasswordContainsUpperLowerAndNumber(string password)
+        {
+            if (password.Any(char.IsUpper)&& password.Any(char.IsLower) && password.Any(char.IsDigit))
             {
-                throw new NotImplementedException();
+                return true;
             }
 
+            return false;
+        }
             public char GetFirstLetterOfString(string val)
             {
-                throw new NotImplementedException();
+            return val[0];
             }
 
             public char GetLastLetterOfString(string val)
             {
-                throw new NotImplementedException();
+            return val.Last();
+            
             }
 
             public decimal Divide(decimal dividend, decimal divisor)
             {
-                throw new NotImplementedException();
+            if (divisor == 0)
+                return 0;
+            return dividend / divisor;
             }
 
             public int LastMinusFirst(int[] nums)
             {
-                throw new NotImplementedException();
+
+            int x = nums.Last() - nums[0];
+            return x;
             }
 
             public int[] GetOddsBelow100()
